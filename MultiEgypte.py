90abcf73ab4f2d0763c010e0e9584c3779098062
AKOKA <akoka@et.esiea.fr>
2022-03-01 11:44:19 +0100

    FInalisation du produit egyptien (erreur précédente :indentation)

--- MultiEgypte.py
+++ MultiEgypte.py
@@ -1,13 +1,13 @@
-def Egypt(a,b):
- A = a
- B = b
- S = 0
- while A != 0:
-    if A % 2 == 1:
-     S = S+B
-     A = A//2
-    B = B+B
- return(S)
+def Egypt():
+   A = int(input("Entrer un nombre: "))
+   B = int(input("Entrer un autre nombre: "))
+   S = 0
+   while A != 0:
+      if A % 2 == 1:
+         S = S+B
+      A = A//2
+      B = B+B
+   return(S)
 
-print(Egypt(25, 16))
+print(Egypt())
 
