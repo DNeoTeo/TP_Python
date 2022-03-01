70908100224f5635143ea1b5b5b027fce854b673
AKOKA <akoka@et.esiea.fr>
2022-03-01 10:40:33 +0100

    Syracuse

---
+++ syracuse.py
@@ -0,0 +1,13 @@
+
+def syracuse(N):
+    u = N
+    n=0
+    while(u!=1): 
+        if u%2==0: 
+            u = u//2 
+        else:
+            u = 3*u+1
+        n += 1
+    return n
+
+print(syracuse(int(input("n ="))))
