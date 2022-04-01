b68075d38e9b847f9970b53df2ec6f5bbf4b18f2
AKOKA <akoka@et.esiea.fr>
2022-04-01 13:18:22 +0200

    Ajout TP3

--- TP3/client3.py
+++ TP3/client3.py
@@ -10,7 +10,7 @@ from PyQt5.QtWidgets import (
 from PyQt5.Qt import QUrl, QDesktopServices
 import requests
 import sys
-import webbrowser
+import socket
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -22,41 +22,47 @@ class MainWindow(QWidget):
         self.setFixedSize(400, 400)
         self.label1 = QLabel("Enter your ip:", self)
         self.text1 = QLineEdit(self)
+        self.text1.setText("localhost:8000")
         self.text1.move(15, 30)
         self.label2 = QLabel("Enter your API key Shodan:", self)
         self.label2.move(10, 60)
         self.text2 = QLineEdit(self)
         self.text2.move(15, 90)
-        self.label3 = QLabel("Enter your ip to find its location:", self)
+        self.label3 = QLabel("Enter the ip to find its location:", self)
         self.label3.move(10, 120)
         self.text3 = QLineEdit(self)
         self.text3.move(15, 150)
+        self.label4 = QLabel("Enter the website to find its location", self)
+        self.label4.move(200, 120)
+        self.text4 = QLineEdit(self)
+        self.text4.move(200, 150)
         self.loc_IP = QLabel("IP: ", self)
-        self.loc_IP.move(200, 150)
+        self.loc_IP.move(200, 190)
         self.loc_orga = QLabel("Organisation: ", self)
-        self.loc_orga.move(200, 180)
+        self.loc_orga.move(200, 210)
         self.loc_country = QLabel("Country: ", self)
-        self.loc_country.move(200, 210)
+        self.loc_country.move(200, 230)
         self.loc_lat = QLabel("Latitude: ", self)
-        self.loc_lat.move(200, 230)
+        self.loc_lat.move(200, 250)
         self.loc_long = QLabel("Longitude: ", self)
-        self.loc_long.move(200, 260)
+        self.loc_long.move(200, 270)
         self.button = QPushButton("Send", self)
-        self.button.move(10, 180)
+        self.button.move(10, 200)
 
         self.button.clicked.connect(self.on_click)
-        self.button.pressed.connect(self.on_click)
+        #self.button.pressed.connect(self.on_click)
         self.show()
 
     def on_click(self):
         hostname = self.text1.text()
         api_key =  self.text2.text()
         ip_loc = self.text3.text()
+        dns_loc = self.text4.text()
 
-        if (hostname == "" or api_key == "" or ip_loc == "") :
+        if hostname == "" or api_key == "" or not((ip_loc == "") ^ (dns_loc == "")) :
             QMessageBox.about(self, "Error", "Please fill the field")
         else:
-            resultserv = self.__query(hostname, api_key, ip_loc)
+            resultserv = self.__query(hostname, api_key, ip_loc, dns_loc)
             if resultserv:
                 self.loc_IP.setText("IP: %s"%(resultserv["IP"]))
                 self.loc_IP.adjustSize()
@@ -70,17 +76,18 @@ class MainWindow(QWidget):
                 self.loc_long.adjustSize()
                 self.show()
                 url = "https://www.openstreetmap.org/?mlat=%s&mlon=%s#map=12" % (resultserv["Latitude"],resultserv["Longitude"])
-                webbrowser.open(url)
-                
+                QDesktopServices.openUrl(QUrl(url))
 
-    def __query(self, hostname, api_key, ip_loc):
+    def __query(self, hostname, api_key, ip_loc, dns_loc):
+        if(dns_loc != "" and ip_loc == ""):
+            ip_loc = socket.gethostbyname(dns_loc)
         url = "http://%s/ip/%s?key=%s" % (hostname,ip_loc,api_key)
+        print(url)
         r = requests.get(url)
         if r.status_code == requests.codes.NOT_FOUND:
             QMessageBox.about(self, "Error", "IP not found")
         if r.status_code == requests.codes.OK:
             return r.json()
-            
             
 
 if __name__ == "__main__":
--- TP3/webserv.py
+++ TP3/webserv.py
@@ -1,9 +1,9 @@
 from typing import Optional
 from fastapi import FastAPI
-
 from shodan import Shodan
 
 app = FastAPI()
+
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
